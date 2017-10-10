97be096e6caf360a90e5a8cbc32a9b5fa4c4cd87
AnhQuocIT <32187187+AnhQuocIT@users.noreply.github.com>
2017-10-10 19:56:10 +0700

    Add files via upload

--- QL_HOCSINH/src/ql_hocsinh/DbConnect.java
+++ QL_HOCSINH/src/ql_hocsinh/DbConnect.java
@@ -5,10 +5,40 @@
  */
 package ql_hocsinh;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author User
  */
 public class DbConnect {
+    Connection con;
+    public DbConnect (){ 
+        try {
+            String URL = "jdbc:sqlserver://ANHQUOC-PC\\ANHQUOCPC:1433;databaseName=QL_HOCSINH;user=anhquoc;password=123";
+            con = DriverManager.getConnection(URL);
+        } catch (SQLException ex) {
+            Logger.getLogger(DbConnect.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public ResultSet getData(String stringSQL){
+        ResultSet rs = null;
+        Statement state;
+        try {
+            state = con.createStatement();
+            rs = state.executeQuery(stringSQL);
+        } catch (SQLException ex) {
+            Logger.getLogger(DbConnect.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return rs;
+    }
+    
     
 }
